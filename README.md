e8671cc1be8706e58834b4131083e419900e0a79
Chi-Hung Chang <chihung861224@gmail.com>
2020-05-16 15:11:59 +0800

    new data files and README

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # CSL_Proj
 NCCU 2020 Spring - Computer Science Logic Term Project
+
+## Instructor
+<h2>Hen-Hsuan Huang</h2>
+<p>
+Assistant Professor
+Dept. of Computer Science
+<strong>National Chengchi University</strong>
+</p>
+
+## Student
+<h2>Chi-Hung Chang</h2>
+<p>
+Senior
+Dept. of Computer Science
+<strong>National Chengchi University</strong>
+</p>
+
+<h2>Pei-Yun Kuo</h2>
+<p>
+Freshman
+Dept. of Computer Science
+<strong>National Chengchi University</strong>
+</p>
+
--- doc2json.py
+++ doc2json.py
@@ -4,7 +4,10 @@ f = open('dataset.txt', 'r')
 #w = open('col_1.json', 'w')
 #w = open('col_2.json', 'w')
 #w = open('col_3.json', 'w')
-w = open('data.json', 'w')
+#w = open('data.json', 'w')
+#w = open('sentence.json', 'w')
+#w = open('reason.json', 'w')
+w = open('result.json', 'w')
 
 count = 0
 
@@ -23,10 +26,8 @@ for i in range(1, 1751):
     E = f.readline().split('\n')[0]
     F = f.readline().split('\n')[0]
 
-    print('{"_id": ' + str(count) + ', "col_A": "' + A + '", "col_B": "' + B + '", "col_C": "' + C
-            + '", "col_D": "' + D + '", "col_E": "' + E + '", "col_F": "' + F + '"}\n')
-    w.write('{"_id": ' + str(count) + ', "col_A": "' + A + '", "col_B": "' + B + '", "col_C": "' + C
-            + '", "col_D": "' + D + '", "col_E": "' + E + '", "col_F": "' + F + '"}\n')
+    print('{"_id": ' + str(count) + ', "result": "' + F + '"}')
+    w.write('{"_id": ' + str(count) + ', "result": "' + F + '"}\n')
 
 #   f.readline()
 #   f.readline()
