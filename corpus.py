6f475ee951f376cd77962df085d87f4c58c18626
Chi-Hung Chang <chihung861224@gmail.com>
2020-05-20 12:59:35 +0800

    corpus text-to function

--- corpus.py
+++ corpus.py
@@ -31,3 +31,33 @@ def getPair(term):
 
     return pairs
 
+def text2text(text, term):
+    f = open('parseEntity.json')
+
+    array = json.load(f)
+    words = []
+
+    for item in array:
+        if text in item['cause']:
+            for word in item[term]:
+                if word not in words:
+                    words.append(word)
+
+    return words
+
+def text2pair(text, term):
+    f = open('parseCombination.json')
+
+    array = json.load(f)
+    pairs = []
+
+    for item in array:
+        for pair in item['cause']:
+            if text in pair:
+                for words in item[term]:
+                    if words not in pairs:
+                        pairs.append(words)
+                break
+
+    return pairs
+
